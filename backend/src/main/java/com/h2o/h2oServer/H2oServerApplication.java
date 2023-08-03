47bfbe70c78c56f0934a841bf34e96e4624a9a50
yohwan <dldyghks951@gmail.com>
2023-08-03 13:02:06 +0900

    feat: ping API 구현

--- backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
+++ backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
@@ -2,12 +2,22 @@ package com.h2o.h2oServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@RestController
 public class H2oServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2oServerApplication.class, args);
 	}
 
+	@GetMapping("/ping")
+	public ResponseEntity ping() {
+		return ResponseEntity.ok("pong");
+	}
 }
