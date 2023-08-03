1e1c968f67756d3060b7f39f9b4c9c8f3a99a5c5
yohwan <dldyghks951@gmail.com>
2023-08-03 12:55:02 +0900

    feat: 스프링부트 프로젝트 초기화

---
+++ backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
@@ -0,0 +1,13 @@
+package com.h2o.h2oServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2oServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2oServerApplication.class, args);
+	}
+
+}
