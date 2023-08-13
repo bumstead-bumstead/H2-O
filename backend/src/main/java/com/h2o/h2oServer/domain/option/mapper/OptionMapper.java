0f15102ae92612758c9f945d8a024fc152ec5ec5
yohwan <dldyghks951@gmail.com>
2023-08-14 01:25:41 +0900

    feat: OptionMapper.java - findOption 쿼리 mapper 클래스 구현

---
+++ backend/src/main/java/com/h2o/h2oServer/domain/option/mapper/OptionMapper.java
@@ -0,0 +1,12 @@
+package com.h2o.h2oServer.domain.option.mapper;
+
+import com.h2o.h2oServer.domain.option.entity.HashTagEntity;
+import com.h2o.h2oServer.domain.option.entity.OptionEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OptionMapper {
+    OptionEntity findOption(Long optionId, Long trimId);
+}
