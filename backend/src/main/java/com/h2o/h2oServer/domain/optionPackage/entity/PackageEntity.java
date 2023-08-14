645a5f7be6f2302f2460e225446379563d3f2143
yohwan <dldyghks951@gmail.com>
2023-08-14 15:15:06 +0900

    test: PackageEntity - package 정보 조회 쿼리 결과 저장을 위한 entity 클래스 구현

---
+++ backend/src/main/java/com/h2o/h2oServer/domain/optionPackage/entity/PackageEntity.java
@@ -0,0 +1,15 @@
+package com.h2o.h2oServer.domain.optionPackage.entity;
+
+import com.h2o.h2oServer.domain.option.entity.enums.OptionCategory;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class PackageEntity {
+    private Long id;
+    private String name;
+    private OptionCategory category;
+    private Integer price;
+    private Float choiceRatio;
+}
