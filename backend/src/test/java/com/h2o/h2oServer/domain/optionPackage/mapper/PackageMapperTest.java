ecb4baee9757cad0c86ae41c1aefd78231604389
yohwan <dldyghks951@gmail.com>
2023-08-14 15:49:48 +0900

    test: PackageMapperTest - findPackage 테스트 코드 구현

--- backend/src/test/java/com/h2o/h2oServer/domain/optionPackage/mapper/PackageMapperTest.java
+++ backend/src/test/java/com/h2o/h2oServer/domain/optionPackage/mapper/PackageMapperTest.java
@@ -2,6 +2,8 @@ package com.h2o.h2oServer.domain.optionPackage.mapper;
 
 import com.h2o.h2oServer.domain.option.entity.HashTagEntity;
 import com.h2o.h2oServer.domain.option.entity.enums.HashTag;
+import com.h2o.h2oServer.domain.option.entity.enums.OptionCategory;
+import com.h2o.h2oServer.domain.optionPackage.entity.PackageEntity;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,11 +21,30 @@ class PackageMapperTest {
     SoftAssertions softly = new SoftAssertions();
 
     @Test
+    @DisplayName("존재하는 trim, package에 대해서 유효한 PackageEntity 객체를 반환한다.")
+    @Sql("classpath:db/package-data.sql")
     void findPackage() {
+        //given
+        Long packageId = 1L;
+        Long trimId = 2L;
+        PackageEntity expectedPackageEntity = PackageEntity.builder()
+                .name("Package 1")
+                .category(OptionCategory.DETAILED_ITEM)
+                .choiceRatio(0.7f)
+                .price(500)
+                .build();
+
+        //when
+        PackageEntity actualPackageEntity = packageMapper.findPackage(trimId, packageId);
+
+        //then
+        softly.assertThat(actualPackageEntity).as("유효한 데이터가 매핑되었는지 확인").isNotNull();
+        softly.assertThat(actualPackageEntity).as("데이터베이스에 존재하는 데이터인지 확인").isEqualTo(expectedPackageEntity);
+        softly.assertAll();
     }
 
     @Test
-    @DisplayName("존재하는 option에 대해서 유효한 HashTagEntity 객체를 반환한다.")
+    @DisplayName("존재하는 package에 대해서 유효한 HashTagEntity 객체를 반환한다.")
     @Sql("classpath:db/package-hashtag-data.sql")
     void findHashTag() {
         //given
