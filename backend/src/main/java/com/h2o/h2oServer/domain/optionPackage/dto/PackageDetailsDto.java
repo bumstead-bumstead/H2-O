10a76b4bee1a958a1e0707d447ad7cb67da04aa6
yohwan <dldyghks951@gmail.com>
2023-08-14 16:52:55 +0900

    feat: PackageDetailsDto - 패키지 세부 정보 반환을 위한 DTO 구현

---
+++ backend/src/main/java/com/h2o/h2oServer/domain/optionPackage/dto/PackageDetailsDto.java
@@ -0,0 +1,36 @@
+package com.h2o.h2oServer.domain.optionPackage.dto;
+
+import com.h2o.h2oServer.domain.option.dto.OptionDetailsDto;
+import com.h2o.h2oServer.domain.option.entity.HashTagEntity;
+import com.h2o.h2oServer.domain.option.entity.enums.HashTag;
+import com.h2o.h2oServer.domain.optionPackage.entity.PackageEntity;
+import lombok.Builder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+public class PackageDetailsDto {
+    private String name;
+    private String category;
+    private Integer price;
+    private Integer choiceRatio;
+    private List<String> hashTags;
+    private List<OptionDetailsDto> components;
+
+    public static PackageDetailsDto of(PackageEntity packageEntity,
+                                       List<HashTagEntity> hashTagEntities,
+                                       List<OptionDetailsDto> optionDetailsDtos) {
+        return PackageDetailsDto.builder()
+                .name(packageEntity.getName())
+                .category(packageEntity.getCategory().getLabel())
+                .price(packageEntity.getPrice())
+                .choiceRatio(Math.round(packageEntity.getChoiceRatio() * 100))
+                .hashTags(hashTagEntities.stream()
+                        .map(HashTagEntity::getName)
+                        .map(HashTag::getLabel)
+                        .collect(Collectors.toList()))
+                .components(optionDetailsDtos)
+                .build();
+    }
+}
