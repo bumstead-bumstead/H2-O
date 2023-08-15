c77152be4d63c63cf1aff01a742072f5f52f9b54
yohwan <dldyghks951@gmail.com>
2023-08-15 22:55:30 +0900

    feat: api 태그, 설명 작성

--- backend/src/main/java/com/h2o/h2oServer/domain/optionPackage/api/PackageController.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/optionPackage/api/PackageController.java
@@ -2,6 +2,8 @@ package com.h2o.h2oServer.domain.optionPackage.api;
 
 import com.h2o.h2oServer.domain.optionPackage.application.PackageService;
 import com.h2o.h2oServer.domain.optionPackage.dto.PackageDetailsDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "옵션")
 public class PackageController {
     private final PackageService packageService;
 
+    @ApiOperation(value = "패키지 세부 정보 조회", notes = "trim_id, package_id를 기준으로 패키지의 세부 정보를 반환하는 API")
     @GetMapping("trim/{trimId}/package/{packageId}")
     public PackageDetailsDto getPackageInformation(@PathVariable Long trimId, @PathVariable Long packageId) {
         return packageService.findPackageInformation(trimId, packageId);
--- backend/src/main/java/com/h2o/h2oServer/domain/options/api/OptionsController.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/options/api/OptionsController.java
@@ -3,6 +3,8 @@ package com.h2o.h2oServer.domain.options.api;
 import com.h2o.h2oServer.domain.options.application.OptionsService;
 import com.h2o.h2oServer.domain.options.dto.TrimDefaultOptionDto;
 import com.h2o.h2oServer.domain.options.dto.TrimExtraOptionDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +16,12 @@ import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "옵션")
 public class OptionsController {
 
     private final OptionsService optionsService;
 
+    @ApiOperation(value = "트림 추가 옵션 조회", notes = "trim_id를 기준으로 모든 추가 옵션 정보를 반환하는 API")
     @GetMapping("/trim/{trimId}/extra-option")
     public List<TrimExtraOptionDto> getExtraOptions(@PathVariable Long trimId) {
         List<TrimExtraOptionDto> trimPackages = optionsService.findTrimPackages(trimId);
@@ -27,6 +31,7 @@ public class OptionsController {
                 .collect(Collectors.toList());
     }
 
+    @ApiOperation(value = "트림 기본 옵션 조회", notes = "trim_id를 기준으로 모든 기본 옵션 정보를 반환하는 API")
     @GetMapping("/trim/{trimId}/default-option")
     public List<TrimDefaultOptionDto> getDefaultOptions(@PathVariable Long trimId) {
         return optionsService.findTrimDefaultOptions(trimId);
