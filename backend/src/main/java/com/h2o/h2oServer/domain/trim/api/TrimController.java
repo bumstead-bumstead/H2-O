6d4229696197d0908cefe3dc938cec1df12e585d
yohwan <dldyghks951@gmail.com>
2023-08-15 16:54:52 +0900

    test: TrimController - 가격 범위 api 엔드포인트 구현

--- backend/src/main/java/com/h2o/h2oServer/domain/trim/api/TrimController.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/trim/api/TrimController.java
@@ -3,6 +3,7 @@ package com.h2o.h2oServer.domain.trim.api;
 import com.h2o.h2oServer.domain.trim.application.TrimService;
 import com.h2o.h2oServer.domain.trim.dto.ExternalColorDto;
 import com.h2o.h2oServer.domain.trim.dto.InternalColorDto;
+import com.h2o.h2oServer.domain.trim.dto.PriceRangeDto;
 import com.h2o.h2oServer.domain.trim.dto.TrimDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +18,12 @@ public class TrimController {
 
     private final TrimService trimService;
 
-    @GetMapping("vehicle/{vehicleId}/trim")
+    @GetMapping("/vehicle/{vehicleId}/trim")
     public List<TrimDto> getTrimInformation(@PathVariable Long vehicleId) {
         return trimService.findTrimInformation(vehicleId);
     }
 
-    @GetMapping("trim/{trimId}/external-color")
+    @GetMapping("/trim/{trimId}/external-color")
     public List<ExternalColorDto> getExternalColorInformation(@PathVariable Long trimId) {
         return trimService.findExternalColorInformation(trimId);
     }
@@ -30,5 +31,10 @@ public class TrimController {
     @GetMapping("/trim/{trimId}/internal-color")
     public List<InternalColorDto> getInternalColorInformation(@PathVariable Long trimId) {
         return trimService.findInternalColorInformation(trimId);
+    }
+
+    @GetMapping("/trim/{trimId}/price-range")
+    public PriceRangeDto getPriceRange(@PathVariable Long trimId) {
+        return trimService.findPriceRange(trimId);
     }
 }
