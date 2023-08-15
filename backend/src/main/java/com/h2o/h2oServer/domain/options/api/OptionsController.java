8585f98683a4cc7ddf017689792e635907502233
yohwan <dldyghks951@gmail.com>
2023-08-15 23:03:26 +0900

    feat: api parameter 태그 작성

--- backend/src/main/java/com/h2o/h2oServer/domain/options/api/OptionsController.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/options/api/OptionsController.java
@@ -4,6 +4,7 @@ import com.h2o.h2oServer.domain.options.application.OptionsService;
 import com.h2o.h2oServer.domain.options.dto.TrimDefaultOptionDto;
 import com.h2o.h2oServer.domain.options.dto.TrimExtraOptionDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,7 @@ public class OptionsController {
     private final OptionsService optionsService;
 
     @ApiOperation(value = "트림 추가 옵션 조회", notes = "trim_id를 기준으로 모든 추가 옵션 정보를 반환하는 API")
+    @ApiImplicitParam(name = "trimId", value = "트림 인덱스 번호")
     @GetMapping("/trim/{trimId}/extra-option")
     public List<TrimExtraOptionDto> getExtraOptions(@PathVariable Long trimId) {
         List<TrimExtraOptionDto> trimPackages = optionsService.findTrimPackages(trimId);
@@ -32,6 +34,7 @@ public class OptionsController {
     }
 
     @ApiOperation(value = "트림 기본 옵션 조회", notes = "trim_id를 기준으로 모든 기본 옵션 정보를 반환하는 API")
+    @ApiImplicitParam(name = "trimId", value = "트림 인덱스 번호")
     @GetMapping("/trim/{trimId}/default-option")
     public List<TrimDefaultOptionDto> getDefaultOptions(@PathVariable Long trimId) {
         return optionsService.findTrimDefaultOptions(trimId);
--- backend/src/main/java/com/h2o/h2oServer/domain/trim/api/TrimController.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/trim/api/TrimController.java
@@ -6,6 +6,7 @@ import com.h2o.h2oServer.domain.trim.dto.ExternalColorDto;
 import com.h2o.h2oServer.domain.trim.dto.InternalColorDto;
 import com.h2o.h2oServer.domain.trim.dto.TrimDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,25 +23,29 @@ public class TrimController {
     private final TrimService trimService;
 
     @ApiOperation(value = "차량의 트림 정보 조회", notes = "car_id를 기준으로 모든 트림 정보를 반환하는 API")
+    @ApiImplicitParam(name = "vehicleId", value = "차종 인덱스 번호")
     @GetMapping("vehicle/{vehicleId}/trim")
     public List<TrimDto> getTrimInformation(@PathVariable Long vehicleId) {
         return trimService.findTrimInformation(vehicleId);
     }
 
-    @GetMapping("trim/{trimId}/external-color")
     @ApiOperation(value = "트림의 외부 색상 정보 조회", notes = "trim_id를 기준으로 모든 외부 색상 정보를 반환하는 API")
+    @ApiImplicitParam(name = "trimId", value = "트림 인덱스 번호")
+    @GetMapping("trim/{trimId}/external-color")
     public List<ExternalColorDto> getExternalColorInformation(@PathVariable Long trimId) {
         return trimService.findExternalColorInformation(trimId);
     }
 
-    @GetMapping("/trim/{trimId}/internal-color")
     @ApiOperation(value = "트림의 내부 색상 정보 조회", notes = "trim_id를 기준으로 모든 내부 색상 정보를 반환하는 API")
+    @ApiImplicitParam(name = "trimId", value = "트림 인덱스 번호")
+    @GetMapping("/trim/{trimId}/internal-color")
     public List<InternalColorDto> getInternalColorInformation(@PathVariable Long trimId) {
         return trimService.findInternalColorInformation(trimId);
     }
 
-    @GetMapping("/trim/{trimId}/default-composition")
     @ApiOperation(value = "트림의 기본 구성 정보 조회", notes = "구성을 선택하지 않았을 경우의 기본적인 모델타입, 색상 구성을 반환하는 API")
+    @ApiImplicitParam(name = "trimId", value = "트림 인덱스 번호")
+    @GetMapping("/trim/{trimId}/default-composition")
     public DefaultTrimCompositionDto getDefaultTrimComposition(@PathVariable Long trimId) {
         return trimService.findDefaultComposition(trimId);
     }
