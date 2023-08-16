b79504e9d65850ea2e8639b4db669bc229a90218
yohwan <dldyghks951@gmail.com>
2023-08-16 16:41:39 +0900

    feat: ErrorResponse - 예외 상황 응답을 위한 DTO 구현

---
+++ backend/src/main/java/com/h2o/h2oServer/global/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.h2o.h2oServer.global.exception;
+
+public class ErrorResponse {
+    private String message;
+
+    private ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public static ErrorResponse of(String message) {
+        return new ErrorResponse(message);
+    }
+}
