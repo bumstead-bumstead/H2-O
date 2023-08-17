946d93282d5373d26fb3fed0f10220f7f432affb
yohwan <dldyghks951@gmail.com>
2023-08-17 14:58:36 +0900

    feat: TrimMapper.findQuantityBetween - 가격 범위 내 출고량 계산 쿼리 작성

--- backend/src/main/java/com/h2o/h2oServer/domain/trim/mapper/TrimMapper.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/trim/mapper/TrimMapper.java
@@ -24,7 +24,7 @@ public interface TrimMapper {
     List<ExternalColorEntity> findExternalColor(Long id);
 
     Integer findMaximumComponentPrice(Long id);
-  
+
     InternalColorEntity findDefaultInternalColor(Long id);
 
     ExternalColorEntity findDefaultExternalColor(Long id);
