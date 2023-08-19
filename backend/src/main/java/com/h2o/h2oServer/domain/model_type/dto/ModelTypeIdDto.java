fa01989f71f7a241ef04320600fc99c12335a2dd
yohwan <dldyghks951@gmail.com>
2023-08-19 23:15:30 +0900

    chore: 어노테이션 추가, 개행 수정

--- backend/src/main/java/com/h2o/h2oServer/domain/model_type/dto/ModelTypeIdDto.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/model_type/dto/ModelTypeIdDto.java
@@ -1,8 +1,10 @@
 package com.h2o.h2oServer.domain.model_type.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ModelTypeIdDto {
     private Long powertrainId;
     private Long bodytypeId;
--- backend/src/main/java/com/h2o/h2oServer/domain/quotation/dto/QuotationRequestDto.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/quotation/dto/QuotationRequestDto.java
@@ -1,11 +1,13 @@
 package com.h2o.h2oServer.domain.quotation.dto;
 
 import com.h2o.h2oServer.domain.model_type.dto.ModelTypeIdDto;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class QuotationRequestDto {
     private Long carId;
     private Long trimId;
