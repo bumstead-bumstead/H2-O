2e08305da4c10302cd149e22ef03d4d1398a7000
yohwan <dldyghks951@gmail.com>
2023-08-19 15:27:44 +0900

    feat: QuotationController - 유사 견적 데이터 조회 엔드포인트 구현

--- backend/src/main/java/com/h2o/h2oServer/domain/quotation/api/QuotationController.java
+++ backend/src/main/java/com/h2o/h2oServer/domain/quotation/api/QuotationController.java
@@ -3,22 +3,30 @@ package com.h2o.h2oServer.domain.quotation.api;
 import com.h2o.h2oServer.domain.quotation.application.QuotationService;
 import com.h2o.h2oServer.domain.quotation.dto.QuotationResponseDto;
 import com.h2o.h2oServer.domain.quotation.dto.QuotationRequestDto;
+import com.h2o.h2oServer.domain.quotation.dto.SimilarQuotationDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/quotation")
 @RequiredArgsConstructor
 public class QuotationController {
 
     private final QuotationService quotationService;
 
-    @PostMapping("/quotation")
+    @PostMapping
     public ResponseEntity<QuotationResponseDto> saveQuotation(@RequestBody QuotationRequestDto quotationRequestDto) {
         QuotationResponseDto quotationResponseDto = quotationService.saveQuotation(quotationRequestDto);
         return new ResponseEntity<>(quotationResponseDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/similar")
+    public List<SimilarQuotationDto> getSimilarQuotations(@RequestBody QuotationRequestDto quotationRequestDto) {
+        return quotationService.findSimilarQuotations(quotationRequestDto);
     }
 }
