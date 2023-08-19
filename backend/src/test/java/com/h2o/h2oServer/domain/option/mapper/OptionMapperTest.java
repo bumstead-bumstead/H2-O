51e4441c5db77bb28cfa44fcf1db3124b8d722f5
yohwan <dldyghks951@gmail.com>
2023-08-20 00:19:09 +0900

    test: OptionMapperTest 리팩토링

--- backend/src/test/java/com/h2o/h2oServer/domain/option/mapper/OptionMapperTest.java
+++ backend/src/test/java/com/h2o/h2oServer/domain/option/mapper/OptionMapperTest.java
@@ -6,6 +6,7 @@ import com.h2o.h2oServer.domain.option.entity.HashTagEntity;
 import com.h2o.h2oServer.domain.option.entity.OptionDetailsEntity;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ class OptionMapperTest {
 
     @Autowired
     private OptionMapper optionMapper;
-    private SoftAssertions softly = new SoftAssertions();
+    private final SoftAssertions softly = new SoftAssertions();
 
     @Test
     @DisplayName("존재하는 trim, option에 대해서 유효한 OptionEntity 객체를 반환한다.")
@@ -52,39 +53,38 @@ class OptionMapperTest {
         List<HashTagEntity> actualHashTagEntities = optionMapper.findHashTag(optionId);
 
         //then
-        softly.assertThat(actualHashTagEntities).as("유효한 데이터가 매핑되었는지 확인").isNotEmpty();
-        softly.assertThat(actualHashTagEntities).as("optionId에 해당하는 HashTagEntity 객체가 모두 매핑되었는지 확인")
-                .contains(expectedHashTagEntities.get(0))
-                .contains(expectedHashTagEntities.get(1))
-                .contains(expectedHashTagEntities.get(2));
-        softly.assertAll();
+        assertThat(actualHashTagEntities).as("optionId에 해당하는 HashTagEntity 객체가 모두 매핑되었는지 확인")
+                .containsAll(expectedHashTagEntities);
     }
 
-    @Test
-    @DisplayName("존재하는 옵션인 경우 true를 반환한다.")
+    @Nested
+    @DisplayName("존재 여부 확인 쿼리 테스트")
     @Sql("classpath:db/option/option-data.sql")
-    void checkIfOptionExists() {
-        //given
-        Long optionId = 1L;
+    class CheckIfOptionExists {
+        @Test
+        @DisplayName("존재하는 옵션인 경우 true를 반환한다.")
+        void checkIfOptionExists() {
+            //given
+            Long optionId = 1L;
 
-        //when
-        Boolean isExists = optionMapper.checkIfOptionExists(optionId);
+            //when
+            Boolean isExists = optionMapper.checkIfOptionExists(optionId);
 
-        //then
-        assertThat(isExists).isTrue();
-    }
+            //then
+            assertThat(isExists).isTrue();
+        }
 
-    @Test
-    @DisplayName("존재하지 않는 옵션인 경우 false를 반환한다.")
-    @Sql("classpath:db/option/option-data.sql")
-    void checkIfOptionExistsFalse() {
-        //given
-        Long optionId = 5L;
+        @Test
+        @DisplayName("존재하지 않는 옵션인 경우 false를 반환한다.")
+        void checkIfOptionExistsFalse() {
+            //given
+            Long optionId = 5L;
 
-        //when
-        Boolean isExists = optionMapper.checkIfOptionExists(optionId);
+            //when
+            Boolean isExists = optionMapper.checkIfOptionExists(optionId);
 
-        //then
-        assertThat(isExists).isFalse();
+            //then
+            assertThat(isExists).isFalse();
+        }
     }
 }
