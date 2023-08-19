cac1e61344c0bf076c2835d023c0a03dda56cfdb
yohwan <dldyghks951@gmail.com>
2023-08-19 20:28:01 +0900

    test: ImageEntity 테스트 객체 fixture 분리

--- backend/src/test/java/com/h2o/h2oServer/domain/trim/mapper/ExternalColorMapperTest.java
+++ backend/src/test/java/com/h2o/h2oServer/domain/trim/mapper/ExternalColorMapperTest.java
@@ -1,5 +1,6 @@
 package com.h2o.h2oServer.domain.trim.mapper;
 
+import com.h2o.h2oServer.domain.trim.ImageFixture;
 import com.h2o.h2oServer.domain.trim.entity.ImageEntity;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,15 +32,7 @@ class ExternalColorMapperTest {
     void findImages() {
         //given
         Long externalColorId = 1L;
-        ImageEntity expectedImageEntity1 = ImageEntity.builder()
-                .image("image_url_1")
-                .id(1L)
-                .build();
-        ImageEntity expectedImageEntity2 = ImageEntity.builder()
-                .image("image_url_2")
-                .id(2L)
-                .build();
-
+        List<ImageEntity> expectedImageEntities = ImageFixture.generateImageEntityList();
 
         //when
         List<ImageEntity> actualImageEntities = externalColorMapper.findImages(externalColorId);
@@ -48,8 +41,8 @@ class ExternalColorMapperTest {
         softly.assertThat(actualImageEntities).as("유효한 데이터가 매핑되었는지 확인").isNotEmpty();
         softly.assertThat(actualImageEntities).as("유효한 데이터만 매핑되었는지 확인").hasSize(2);
         softly.assertThat(actualImageEntities).as("externalColorId에 해당하는 객체가 모두 매핑되었는지 확인")
-                .contains(expectedImageEntity1)
-                .contains(expectedImageEntity2);
+                .containsAll(expectedImageEntities);
+
         softly.assertAll();
     }
 }
