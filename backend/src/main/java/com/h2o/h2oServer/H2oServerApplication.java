10f2a1473d9c33416e4c79e83a572dc699b7e766
Yohwan Lee <91039622+bumstead-bumstead@users.noreply.github.com>
2023-08-22 16:39:56 +0900

    feat: Redis 테스트용 엔드포인트 구현

--- backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
+++ backend/src/main/java/com/h2o/h2oServer/H2oServerApplication.java
@@ -1,19 +1,23 @@
 package com.h2o.h2oServer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableCaching
 public class H2oServerApplication {
+
+	@Autowired
+	RedisTemplate<String, String> redisTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2oServerApplication.class, args);
@@ -22,6 +26,14 @@ public class H2oServerApplication {
 	@GetMapping("/ping")
 	public ResponseEntity ping() {
 		return ResponseEntity.ok("pong");
+	}
+
+	@GetMapping("/redis-ping")
+	public ResponseEntity redisPing() {
+		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+		valueOperations.set("testKey", "testValue");
+
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
 
